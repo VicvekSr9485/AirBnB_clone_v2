9ffe72b37c4c07528c805a22bf09f9cea8880dc2
VicvekSr <Olamideoso65@gmail.com>
2022-10-14 19:35:30 +0000

    add HBNB page

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" This module defines route in flask. """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ This method returns Hello HBNB page """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_page():
+    """ This method returns HBNB page """
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
