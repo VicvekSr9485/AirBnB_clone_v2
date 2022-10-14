9e301090bf85f1e4f78f2bc58a6a6917f857a8f5
VicvekSr <Olamideoso65@gmail.com>
2022-10-14 19:28:41 +0000

    add init and route modules

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" This module defines route in flask. """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False )
+def hello_hbnb():
+    """ This method returns Hello HBNB page """
+    return "Hello HBNB!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
