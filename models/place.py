906f8159161a4c71ed9fb464a833b5e034c329af
VicvekSr <Olamideoso65@gmail.com>
2022-10-16 10:50:57 +0000

    fix

--- models/place.py
+++ models/place.py
@@ -12,27 +12,27 @@ place_amenity = Table("place_amenity", Base.metadata,
                       Column("place_id", String(60),
                              ForeignKey("places.id"),
                              nullable=False, primary_key=True),
-                      Column("amenity_id", String(60),
-                             ForeignKey("amenities.id"),
-                             nullable=False, primary_key=True))
+#                      Column("amenity_id", String(60),
+#                             ForeignKey("amenities.id"),
+#                             nullable=False, primary_key=True))
 
 
 class Place(BaseModel, Base):
     """ A place to stay """
-    __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey('cities.id', ondelete="CASCADE"),
-                     nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id', ondelete="CASCADE"),
-                     nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms = Column(Integer, nullable=False, default=0)
-    max_guest = Column(Integer, nullable=False, default=0)
-    price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=True)
-    longitude = Column(Float, nullable=True)
-    amenity_ids = []
+#    __tablename__ = 'places'
+#    city_id = Column(String(60), ForeignKey('cities.id', ondelete="CASCADE"),
+#                     nullable=False)
+#    user_id = Column(String(60), ForeignKey('users.id', ondelete="CASCADE"),
+#                     nullable=False)
+ #   name = Column(String(128), nullable=False)
+ #   description = Column(String(1024), nullable=True)
+ #   number_rooms = Column(Integer, nullable=False, default=0)
+ #   number_bathrooms = Column(Integer, nullable=False, default=0)
+  #  max_guest = Column(Integer, nullable=False, default=0)
+ #   price_by_night = Column(Integer, nullable=False, default=0)
+ #   latitude = Column(Float, nullable=True)
+ #   longitude = Column(Float, nullable=True)
+ #   amenity_ids = []
 
     if getenv("HBNB_TYPE_STORAGE") == "db":
         reviews = relationship("Review", backref="place",
